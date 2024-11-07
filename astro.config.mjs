c5e0883350d1ceaa0466279643215823f347ce16
David Leininger <davidleininger@hey.com>
2024-11-06 20:32:40 -0600

    clean up and add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nytimesguild.org/tech/guild-builds",
   integrations: [icon()]
 });
